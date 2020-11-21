d2bc39389035aee2e984ffc959927091ecef6322
Marcin Szymczyk <marcin.szymczyk@aptiv.com>
2020-11-21 16:49:00 +0100

    Chapter 6

--- chapter_4.py
+++ chapter_4.py
@@ -1,5 +1,6 @@
 __all__ = ['Osoba', 'statystyczny_kowalski']
 
+
 class Osoba:
 
     def __init__(self, imie, nazwisko, wiek, praca):
@@ -10,8 +11,30 @@ class Osoba:
         self._stan_konta = 0
         self.hobby = ['literatura', 'film', 'szachy']
 
-    def wyplata(self, kwota):
-        self._stan_konta += kwota
+    @property
+    def praca(self):
+        with open(r'C:\projects\SzkoleniePython2020\testowy\text.txt', 'r') as nasz_plik:
+            self.__praca = nasz_plik.read()
+        return self.__praca
+
+    @praca.setter
+    def praca(self, praca):
+        with open(r'C:\projects\SzkoleniePython2020\testowy\text.txt', 'w') as nasz_plik:
+            nasz_plik.write(praca)
+        self.__praca = praca
+
+
+    def __str__(self):
+        return f'Imię: {self.imie}\nNazwisko: {self.nazwisko}'
+
+    def __repr__(self):
+        return f'Imię: {self.imie}\nNazwisko: {self.nazwisko}\nWiek: {self.wiek}'
+
+    def __getitem__(self, item):
+        return self.hobby[item]
+
+    def __add__(self, other):
+        self._stan_konta += other._stan_konta
 
     def zakupy(self, kwota):
         self._stan_konta -= kwota
@@ -21,6 +44,7 @@ def statystyczny_kowalski():
     kowalski = Osoba('Jan', 'Kowalski', 43, 'Policjant')
     return kowalski
 
+
 class Smieci:
 
     def __init__(self, waga):
@@ -29,6 +53,14 @@ class Smieci:
     def podaj_wage(self):
         print(self.waga_smieci_w_kg)
 
+
 if __name__ == '__main__':
-    s = Smieci(15)
-    s.podaj_wage()
+    o = statystyczny_kowalski()
+    # o._stan_konta = 2
+    #     # o2 = Osoba('Maria', 'Kowalska', 37, 'Malarka')
+    #     # o2._stan_konta = 10
+    #     # print(o._stan_konta)
+    #     # o+o2
+    #     # print(o._stan_konta)
+    o.praca = 'Tytu'
+    print(o.praca)
