c722a99d82cc0f451e16ce6b4135c4a9dab482b5
Marcin Szymczyk <marcin.szymczyk@aptiv.com>
2020-11-22 14:54:55 +0100

    Chapter 9 Pytest Calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+def dodawanie(parametr_1, parametr_2):
+    if type(parametr_1) not in [int,float] or type(parametr_2) not in [int,float]:
+        raise TypeError
+    return parametr_1 + parametr_2
+
+
+def odejmowanie():
+    pass
+
+
+def mnożenie():
+    pass
+
+
+def dzielenie():
+    pass
