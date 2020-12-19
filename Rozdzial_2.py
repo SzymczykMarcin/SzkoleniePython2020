c926cd02db64b3c2a2e6191e56e3992e2ad981f6
Marcin Szymczyk <marcin.szymczyk@aptiv.com>
2020-12-19 12:09:05 +0100

    Rozdzial 2

--- Rozdzial_2.py
+++ Rozdzial_2.py
@@ -1,2 +1,23 @@
-def func_func():
-    print('Ala ma kota.')
+# def func_func(imie, zwierze):
+#     print('{} ma {}.'.format(imie, zwierze))
+#
+#
+# func_func('Ala', 'kota')
+
+
+# def next_func(imie='Ala', zwierze='kota'):
+#     print('{} ma {}.'.format(imie, zwierze))
+#
+# next_func()
+# next_func('Adam', 'psa')
+
+# def adding(var_one, var_two):
+#     return var_one + var_two
+#
+#
+# result = adding(1, 1)
+# print(result)
+# print(adding('Adam', 'psa'))
+
+
+my_list = ['Marcin', 'Jakub', 'Taras', 'Michal']
